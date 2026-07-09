1a233bb9779586f76ff9395500e09af0a6b41b7b
jose Angel <josegallardo.cor@gmail.com>
2026-07-08 23:41:32 -0600

    feat: creacion de dominio de cat

---
+++ CatApiA_02/Domain/Cat.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace CatApiA_02.Domain
+{
+    public class Cat
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTimeOffset DateBirth { get; private set; }
+        public decimal Weight { get; private set; }
+        public decimal Height { get; private set; }
+        public string Breed { get; private set; }
+        private Cat()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            Breed = string.Empty;
+        }
+        private Cat(string name, string description, DateTimeOffset dateBirth, decimal weight, decimal height, string breed)
+        {
+            var isValid = validate(name, description, dateBirth, breed);
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            DateBirth = dateBirth;
+            Weight = weight;
+            Height = height;
+            Breed = breed;
+        }
+        private void Update(string name, string description, DateTimeOffset dateBirth, decimal weight, decimal height, string breed)
+        {
+            var isValid = validate(name, description, dateBirth, breed);
+
+            Name = name;
+            Description = description;
+            DateBirth = dateBirth;
+            Weight = weight;
+            Height = height;
+            Breed = breed;
+        }
+        private bool validate(string name, string description, DateTimeOffset dateBirth, string breed)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Description cannot be null or empty.", nameof(description));
+            if (dateBirth == default)
+                throw new ArgumentException("Date of birth cannot be the default value.", nameof(dateBirth));
+            if (string.IsNullOrWhiteSpace(breed))
+                throw new ArgumentException("Breed cannot be null or empty.", nameof(breed));
+            return true;
+        }
+    }
+}
--- CatApiA_02/Program.cs
+++ CatApiA_02/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//services
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
